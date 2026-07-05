355ec3d67873fa155b87f79fee79dfc5e6d9c1e1
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-05 11:57:49 +0200

    Prepare news API integration

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-STOCK = 'NVDA'
+STOCK_ENDPOINT = 'NVDA'
+NEWS_ENDPOINT = 'https://newsapi.org/v2/everything'
 API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY')
-URL = 'https://www.alphavantage.co/query'
 
-parameters = {
+stock_url = 'https://www.alphavantage.co/query'
+
+stock_parameters = {
     'function': 'TIME_SERIES_DAILY',
-    'symbol': STOCK,
+    'symbol': STOCK_ENDPOINT,
     'outputsize': 'compact',
     'apikey': API_KEY,
 }
 
 response = requests.get(
-    url=URL,
-    params=parameters,
+    url=stock_url,
+    params=stock_parameters,
 )
 response.raise_for_status()
 data = response.json()
@@ -28,6 +30,7 @@ daily_data = data['Time Series (Daily)']
 dates = list(daily_data.keys())
 latest_date = dates[0]
 previous_date = dates[1]
+
 latest_close = float(daily_data[latest_date]['4. close'])
 previous_close = float(daily_data[previous_date]['4. close'])
 
@@ -36,7 +39,7 @@ diff = latest_close - previous_close
 percentage_diff = round(diff / previous_close * 100, 2)
 
 if abs(percentage_diff) > 5:
-    print(f'{STOCK} changed by {percentage_diff}')
+    print(f'{STOCK_ENDPOINT} changed by {percentage_diff}')
 
 
 
