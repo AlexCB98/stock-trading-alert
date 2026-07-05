861540ddbc5bff246eb0c8164f58ee79e6cef1b7
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-05 11:07:33 +0200

    Extract latest and previous stock close prices

--- main.py
+++ main.py
@@ -15,3 +15,25 @@ parameters = {
     'apikey': API_KEY,
 }
 
+response = requests.get(
+    url=URL,
+    params=parameters,
+)
+response.raise_for_status()
+
+data = response.json()
+
+stock_name = data['Meta Data']['2. Symbol']
+
+daily_data = data['Time Series (Daily)']
+
+dates = list(daily_data.keys())
+
+latest_date = dates[0]
+previous_date = dates[1]
+
+latest_close = float(daily_data[latest_date]['4. close'])
+previous_close = float(daily_data[previous_date]['4. close'])
+
+
+
