487b29cb7968a5223af0c479a01d4108ca9aac26
Cocioaba Alexandru Bogdan <lexyus22@gmail.com>
2026-07-05 12:49:32 +0200

    Add email alert for stock news

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+import smtplib
 
 load_dotenv()
 
@@ -9,6 +10,10 @@ COMPANY_NAME = 'NVIDIA'
 
 STOCK_API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY')
 NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
+MY_EMAIL = os.environ.get('MY_EMAIL')
+MY_PASSWORD = os.environ.get('MY_EMAIL_PASSWORD')
+
 
 def get_stock_data():
     stock_url = 'https://www.alphavantage.co/query'
@@ -25,9 +30,9 @@ def get_stock_data():
         params=stock_parameters,
     )
     stock_response.raise_for_status()
-    data = stock_response.json()
 
-    return data
+    return stock_response.json()
+
 
 def get_news_data():
     news_url = 'https://newsapi.org/v2/everything'
@@ -35,6 +40,8 @@ def get_news_data():
     news_parameters = {
         'apiKey': NEWS_API_KEY,
         'qInTitle': COMPANY_NAME,
+        'language': 'en',
+        'sortBy': 'publishedAt',
     }
 
     news_response = requests.get(
@@ -42,11 +49,10 @@ def get_news_data():
         params=news_parameters,
     )
     news_response.raise_for_status()
-    data = news_response.json()
 
-    return data
+    return news_response.json()
 
-news_data = get_news_data()
+
 stock_data = get_stock_data()
 
 stock_name = stock_data['Meta Data']['2. Symbol']
@@ -62,11 +68,34 @@ previous_close = float(daily_data[previous_date]['4. close'])
 diff = latest_close - previous_close
 percentage_diff = round(diff / previous_close * 100, 2)
 
-articles = news_data['articles']
+if percentage_diff > 0:
+    direction = 'UP'
+else:
+    direction = 'DOWN'
 
 if abs(percentage_diff) > 1:
-    print(articles)
+    news_data = get_news_data()
 
+    articles = news_data['articles']
+    three_articles = articles[:3]
 
+    formatted_articles = [
+        f'Headline: {article["title"]}\n'
+        f'Brief: {article["description"]}'
+        for article in three_articles
+    ]
 
+    email_body = '\n\n'.join(formatted_articles)
 
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=(
+                f'Subject: {COMPANY_NAME} {direction} {abs(percentage_diff)}%\n\n'
+                f'{STOCK_SYMBOL} changed by {percentage_diff}%.\n\n'
+                f'{email_body}'
+            )
+        )
